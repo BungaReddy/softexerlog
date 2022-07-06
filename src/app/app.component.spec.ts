dc93450ff870d3daf73bc4bf8858776c8e8dddc6
BungaReddy <bchandravamsireddy@gmail.com>
2022-07-06 17:36:32 +0530

    add files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'softexerlog'`, () => {
+  it(`should have as title 'loginpage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('softexerlog');
+    expect(app.title).toEqual('loginpage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('softexerlog app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('loginpage app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'softexerlog';
+export class AppComponent implements OnInit {
+  title = 'loginpage';
+  form: FormGroup = new FormGroup({
+    Email: new FormControl(''),
+  })
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        Email: ['', Validators.required],
+      })
+    }
+  
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+  
+  
 }
